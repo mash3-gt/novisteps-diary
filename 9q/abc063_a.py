16e1732444b54306f9bf36a6b1d02462b0358db8
mashgt <fg250j@gmail.com>
2025-09-07 16:58:50 +0900

    update

---
+++ 9q/abc063_a.py
@@ -0,0 +1,6 @@
+a,b = map(int, input().split())
+ans = a+b
+if ans >= 10:
+    print('error')
+else:
+    print(ans)
---
+++ 9q/abc087_a.py
@@ -0,0 +1,5 @@
+x = int(input())
+a = int(input())
+b = int(input())
+first = x - a
+print(first % b)
